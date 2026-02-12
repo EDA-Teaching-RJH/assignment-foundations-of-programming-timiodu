e4a99233a14b1b5da2c0fe33b433ebdb929f4a1e
timiodu <eto26@kent.ac.uk>
2026-02-12 03:01:22 +0000

    members!

--- fleet_manager.py
+++ fleet_manager.py
@@ -30,3 +30,34 @@ def display_menu():
 
     return choice
 
+def add_member(names, ranks, divs, ids): 
+     name = input("Enter name: ").strip()
+     valid_ranks = ["Captain", "Commander", "Lt. Commander",
+                   "Lieutenant", "Ensign"]
+     
+     rank = input("Enter rank: ").strip()
+
+     if rank not in valid_ranks:
+        print("Invalid rank. Must be one of:")
+        for r in valid_ranks:
+            print("-", r)
+        return
+
+        division = input("Enter division (Command/Operations/Security/Sciences): ").strip()
+
+        try:
+            new_id = int(input("Enter unique ID number: "))
+        except ValueError:
+            print("ID must be a number.")
+        return
+
+        if new_id in ids:
+            print("Error: ID already exists.")
+        return
+        names.append(name)
+        ranks.append(rank)
+        divs.append(division)
+        ids.append(new_id)
+
+        print("Crew member successfully added.")#
+
